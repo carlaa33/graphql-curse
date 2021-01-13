a31b131d7e6250b1228218ac0da075e9f0030f6f
Carlita <carlareyesgo@gmail.com>
2021-01-13 11:45:31 -0700

    ES6 Import/Export

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello Graphql");
+import myCurrentLocation, { getGreeting, message, name } from "./myModule";
+import myAddFunction, { subtract } from "./math";
+
+console.log(message);
+
+console.log(name);
+
+console.log(myCurrentLocation);
+
+console.log(getGreeting("Maria"));
+
+console.log(myAddFunction(3, 2));
+
+console.log(subtract(5, 1));
